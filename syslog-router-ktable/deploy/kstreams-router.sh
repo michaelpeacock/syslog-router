ea9dc8f131b85fcaa9f30a540dfb2acac5559db2
Peacock, Michael W Jr CTR (USA) <michael.w.peacock4.ctr@army.mil>
2024-04-25 10:23:59 -0400

    updated docs and logging

--- syslog-router-ktable/deploy/kstreams-router.sh
+++ syslog-router-ktable/deploy/kstreams-router.sh
@@ -3,4 +3,4 @@
 export SPRING_CONFIG_NAME=application,routingconfig
 export SPRING_CONFIG_LOCATION=file://$PWD/config/
 
-java -Dlog4j.configuration=file:config/log4j.properties -jar kstreamrouter-1.0.jar
+java -Dlog4j.configuration=file:config/log4j.properties -jar kstreamrouter-join-1.0.jar
